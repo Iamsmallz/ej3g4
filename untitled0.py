1882d864132ba869e55c5f48b517c1850dd9e8a1
Iamsmallz <iami.smallz@gmail.com>
2013-11-21 17:52:38 +0800

    0.3a

--- untitled0.py
+++ untitled0.py
@@ -10,12 +10,12 @@ import datetime
 
 class SDate:
     
-    def __init__(self):
+    def __init__(self): #initial SDate
         self.SD = []
         self.nowyear = int(datetime.datetime.now().strftime('%Y'))
         self.nowmonth = int(datetime.datetime.now().strftime('%m'))
         
-    def ymdatelist(self):
+    def ymdatelist(self): #get year & month value
 
         for x in range(1993,self.nowyear+1): #create all year and month
             if x != self.nowyear:
@@ -37,18 +37,30 @@ class SDate:
                     
 snumber = '2314'
 SPdatalist = []
+templist = []
 SDatelist = SDate()
 SDatelist.ymdatelist()
          
-for sddate in SDatelist.SD: 
+for sddate in SDatelist.SD: #store url data into temp list
     url = 'http://www.twse.com.tw/ch/trading/exchange/STOCK_DAY/STOCK_DAY_print.php?genpage=genpage/Report' \
     + sddate + '/' + sddate +'_F3_1_8_' + snumber + '.php&type=csv'  
-    response = urllib.urlopen(url)  
-    html = response.read()  
-    sp = html.splitlines()
+    response = urllib.urlopen(url)  #open url
+    html = response.read()  #read url data
+    sp = html.splitlines()  #split data into list
+    response.close()        #close url
     del sp[0:2]
     if sp != []:
-        SPdatalist.extend(sp)
-print SPdatalist 
-print "1"
-print SPdatalist[0]
+        templist.extend(sp) #Merge list
+
+for sp in templist: #Transform temp list data into SPdatalist
+    start = sp.find('\"')
+    while start != -1:
+        end = sp.find('\"',start+1)
+        sp = sp[:start] + sp[start+1:end].replace(",","") + sp[end+1:]
+        start = sp.find('\"')
+    SPdatalist.append(sp)
+
+print SPdatalist
+
+sp = SPdatalist[0]
+temp = [idx for (idx, s) in enumerate(sp) if s == ',']
